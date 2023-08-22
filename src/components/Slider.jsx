60a75e4ee8d5e8b983468cd84587bd4a2d499e4e
jrlopes <contacto.juniorlope@gmail.com>
2023-08-22 01:22:15 -0500

    images

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -10,9 +10,9 @@ const Slider = () => {
 
     <section className="flex justify-center relative mb-11">
 
-            <img className=' h-[350px] md:h-[600px] w-full absolute'  src="./public/img/stars.png" alt="" />
+            <img className=' h-[350px] md:h-[600px] w-full absolute'  src="../public/img/stars.png" alt="" />
             <img className=' h-[240px] w-[320px] md:h-[500px] md:w-[700px] justify-center  z-30 absolute '  src="../public/img/portal.png" alt="" />
-            <img className=' mt-[120px] h-[200px] w-[300px] md:mt-[200px] md:h-[350px] md:w-[800px] z-40'  src="./public/img/logo.png" alt="" />
+            <img className=' mt-[120px] h-[200px] w-[300px] md:mt-[200px] md:h-[350px] md:w-[800px] z-40'  src="../public/img/logo.png" alt="" />
 
       
     </section>
